91af54102e487d7d9a53eefc53f67308f94c109f
angel <angel.mora.1@outlook.com>
2023-07-26 14:54:03 -0700

    stuck on calculate grade average

--- src/grade.js
+++ src/grade.js
@@ -2,16 +2,27 @@ class Grade {
     constructor(individualClasses, graduationYear) {
         this.individualClasses = individualClasses,
         this.graduationYear = graduationYear
-        
+
     }
     getStudentCount (grade) {
       let students = 0
-      for (let room in grade.individualClasses){
-        if (grade == grade.individualClasses[room].grade){
-            students = room.students.length
+      for (let room in this.individualClasses){
+        if (grade == this.individualClasses[room].gradeLevel){
+            students += this.individualClasses[room].student.length
         }
       }
       return students;
+    }
+    calculateGradeAverage(grade){
+      let counter = 0
+      let allscores = 0
+      for (let room in this.individualClasses){
+        for(let student of this.individualClasses[room]){
+          counter += student.testScores.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+          allscores += student.testScores.length
+        }
+      }
+      return math.floor(counter/allscores)
     }
 }
     module.exports = Grade;
--- test/grade.test.js
+++ test/grade.test.js
@@ -1,20 +1,29 @@
 const { IndividualClass } = require('../src');
+const { Student } = require('../src');
 
 const Grade = require('../src').Grade;
 
 describe('Grade', () => {
   describe('insantiates a new Grade', () => {
-    const mathClass = new IndividualClass('Math', 10, `Mrs. White`, ['John Doe', `Jane Doe`])
-    const historyClass = new IndividualClass('History', 10, `Mr. Bob`, ['Jim Doe', `Jess Doe`])
+    const jane = new Student('Jane Doe', 15, 10, [90, 80], 2026, [`band`, `baseball`]);
+    const jess = new Student('Jess Doe', 12, 10, [50, 30], 2026, [`band`, `baseball`]);
+    const john = new Student('John Doe', 16, 10, [70, 90], 2026, [`band`, `baseball`]);
+    const jim = new Student('Jim Doe', 17, 10, [100, 100], 2026, [`band`, `baseball`]);
+    const mathClass = new IndividualClass('Math', 10, `Mrs. White`, [john, jim])
+    const historyClass = new IndividualClass('History', 10, `Mr. Bob`, [jane, jess])
     const grade = new Grade([mathClass, historyClass], 1995);
 
+
     test('takes the following arguments: individualClasses, graduationYear', () => {
-      expect(grade.individualClasses).toEqual(['Math', 10, `Mrs. White`, ['John Doe', `Jane Doe`]]);
+      expect(grade.individualClasses).toEqual([mathClass, historyClass]);
       expect(grade.graduationYear).toBe(1995);
-      
+
     })
     test('get student count', () => {
-        expect(grade.getStudentCount(10)).toEqual(2)
+        expect(grade.getStudentCount(10)).toBe(4)
+    })
+    test('calculate grade average', () => {
+        expect(grade.calculateGradeAverage(10)).toBe(76)
     })
 })
 })
